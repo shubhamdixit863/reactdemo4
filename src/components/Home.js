376787d2ffc1299cb7d89b32fec59b2cfeaa8c5e
shubham dixit <shubhamdixit863@gmail.com>
2021-05-25 20:26:38 +0530

    newproject

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// state and props
+
+function Home() {
+    return (
+        <div>
+            <h1 className="heading">Home Component</h1>
+        </div>
+    )
+}
+
+export default Home
